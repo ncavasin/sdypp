75d2c997836eae93d407a93b72cd3ded8d3bfc15
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-09 22:31:33 -0300

    TP1-5 - make more verbose the ClimateStatus toString() method

--- TP1/TP1-5/rmi-server/src/main/java/utils/ClimateStatus.java
+++ TP1/TP1-5/rmi-server/src/main/java/utils/ClimateStatus.java
@@ -3,10 +3,21 @@ package utils;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.StringJoiner;
+
 @Data
 @Builder
 public class ClimateStatus {
     private String country;
     private String temperature;
     private String humidity;
+
+    @Override
+    public String toString() {
+        StringJoiner stringJoiner = new StringJoiner("\n");
+        return stringJoiner.add("Climate conditions in " + country + " are:")
+                .add("\tTemperature: " + temperature)
+                .add("\tHumidity: " + humidity)
+                .toString();
+    }
 }
