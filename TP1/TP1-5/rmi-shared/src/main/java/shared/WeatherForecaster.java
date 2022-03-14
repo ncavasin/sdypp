81195201223d6a05a087d07578a82c917e4179f3
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-13 21:35:33 -0300

    TP1-5 - adding a shared module for the interface sharing

---
+++ TP1/TP1-5/rmi-shared/src/main/java/shared/WeatherForecaster.java
@@ -0,0 +1,24 @@
+package shared;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Due to the fact that this interface extends Remote and exposes methods, any implementer will
+ * be ready to serve them through RMI
+ */
+public interface WeatherForecaster extends Remote {
+
+    // Echo back received message
+    String echo(String msg) throws RemoteException;
+
+    // Let the client know who the implementer is
+    String identifyYourself() throws RemoteException;
+
+    // Let the client know how the climate is at the implementer's location
+    String getClimateConditions() throws RemoteException;
+
+    // Let the client know who the implementer is AND how the climate is at the implementer's location (or so...)
+    String getFullReport() throws RemoteException;
+
+}
