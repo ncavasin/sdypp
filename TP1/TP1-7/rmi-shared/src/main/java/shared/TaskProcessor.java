8126c905021490389eed7ab2b2773c7ad7b53fca
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-20 18:50:05 -0300

    TP1-7: define a task and expose a task processor to be consumed from several clients

--- TP1/TP1-7/rmi-shared/src/main/java/shared/TaskProcessor.java
+++ TP1/TP1-7/rmi-shared/src/main/java/shared/TaskProcessor.java
@@ -2,13 +2,12 @@ package shared;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.List;
 
 /**
  * Due to the fact that this interface extends Remote and exposes methods, any implementer will
  * be ready to serve them through RMI
  */
-public interface VectorOperator extends Remote {
+public interface TaskProcessor extends Remote {
 
     // Echo back received message
     String echo(String msg) throws RemoteException;
@@ -16,8 +15,6 @@ public interface VectorOperator extends Remote {
     // Let the client know who the implementer is
     String identifyYourself() throws RemoteException;
 
-    VectorOperationResultDto addition(List<Float> v1, List<Float> v2) throws RemoteException;
-
-    VectorOperationResultDto subtraction(List<Float> v1, List<Float> v2) throws RemoteException;
+    <T> T executeTask(Task <T> t)  throws RemoteException;;
 
 }
