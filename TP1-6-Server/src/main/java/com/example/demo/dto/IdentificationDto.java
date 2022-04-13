75d1f67c8b061f5fa426819079e52f97c089b16f
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-13 17:26:17 -0300

    TP1-6-Server: seems to be working

---
+++ TP1-6-Server/src/main/java/com/example/demo/dto/IdentificationDto.java
@@ -0,0 +1,33 @@
+package com.example.demo.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.StringJoiner;
+
+@Data
+@Builder
+public class IdentificationDto {
+
+    // socket
+    private String ipAddress;
+    private int port;
+
+    // process ID
+    private long pid;
+
+    // server's name
+    private String name;
+
+    @Override
+    public String toString() {
+        StringJoiner stringJoiner = new StringJoiner(". ");
+        return stringJoiner
+                .add(">> ")
+                .add("Hello! My name is PID " + pid)
+                .add("My friends call me " + name + " (RMI lookup value)")
+                .add("You can find me at " + ipAddress + ":" + port + " (RMI TCP socket)")
+                .add("<<")
+                .toString();
+    }
+}
---
+++ TP1-6-Server/src/main/java/com/example/demo/shared/VectorOperationResultDto.java
@@ -0,0 +1,17 @@
+package com.example.demo.shared;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class VectorOperationResultDto implements Serializable {
+    private List<Float> v1;
+    private List<Float> v2;
+    private List<Float> result;
+}
