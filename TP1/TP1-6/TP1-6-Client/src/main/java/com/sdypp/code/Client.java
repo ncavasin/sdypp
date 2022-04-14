1e5161ed705a48f4091029bc6c3bed3989c37085
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-14 17:16:15 -0300

    TP1-6-Client: update log messages

--- TP1/TP1-6/TP1-6-Client/src/main/java/com/sdypp/code/Client.java
+++ TP1/TP1-6/TP1-6-Client/src/main/java/com/sdypp/code/Client.java
@@ -23,56 +23,63 @@ public class Client {
 
         log.info("Bootstrapping RMI client...");
 
-        // Locate the RMI registry using received socket
-        Registry registry = getRegistry();
+        try {        // Locate the RMI registry using received socket
+            Registry registry = getRegistry();
 
-        // Lookup the exposed weather forecaster object in the registry
-        VectorOperator vectorMath = getVectorMath(registry);
+            try {
+                // Lookup the exposed weather forecaster object in the registry
+                VectorOperator vectorMath = getVectorMath(registry);
 
-        try {
-            String msg = "I am invoking you through RMI";
-            log.info("Client requested echo of <{}>...", msg);
-            log.info("SERVER => {}", vectorMath.echo(msg));
-        } catch (RemoteException e) {
-            log.warn("Remote method invocation failed: {}", e.getMessage());
-            e.printStackTrace();
+                try {
+                    String msg = "I am invoking you through RMI";
+                    log.info("Client requested echo of <{}>...", msg);
+                    log.info("SERVER => {}", vectorMath.echo(msg));
+                } catch (RemoteException e) {
+                    log.warn("Remote method invocation failed: {}", e.getMessage());
+                    e.printStackTrace();
+                }
+
+                try {
+                    log.info("Client asked Server to identify itself...");
+                    log.info("SERVER => {}", vectorMath.identifyYourself());
+                } catch (RemoteException e) {
+                    log.warn("Remote method invocation failed: {}", e.getMessage());
+                    e.printStackTrace();
+                }
+
+                // Define two vectors
+                List<Float> v1 = List.of(9.8F, 5F, 10F, 1F, 1F);
+                log.info("Vector 1 = {}.", v1);
+                List<Float> v2 = List.of(0.2F, 5F, 17F, 1F, 9F);
+                log.info("Vector 2 = {}.\n\n", v2);
+
+                try {
+                    VectorOperationResultDto vectorOperationResultDto = vectorMath.addition(v1, v2);
+                    log.info("Received V1 = {}", vectorOperationResultDto.getV1());
+                    log.info("Received V2 = {}", vectorOperationResultDto.getV2());
+                    log.info("Addition = {}\n\n", vectorOperationResultDto.getResult());
+                } catch (RemoteException e) {
+                    log.warn("Remote method invocation failed: {}", e.getMessage());
+                    e.printStackTrace();
+                }
+
+                try {
+                    VectorOperationResultDto vectorOperationResultDto = vectorMath.subtraction(v1, v2);
+                    log.info("Received V1 = {}", vectorOperationResultDto.getV1());
+                    log.info("Received V2 = {}", vectorOperationResultDto.getV2());
+                    log.info("Subtraction = {}\n\n", vectorOperationResultDto.getResult());
+                } catch (RemoteException e) {
+                    log.warn("Remote method invocation failed: {}", e.getMessage());
+                    e.printStackTrace();
+                }
+
+                System.out.println("Conclusion: despite of the fact that Server modifies the vector's content, as they are passed by value and not by reference, the Client content won't be modified but the result will due to the Server is using those modified values! ");
+            } catch (Exception e) {
+                log.error("Could not lookup the exposed weather forecaster object in the registry. Status: {}", e.getMessage());
+            }
+        } catch (Exception e) {
+            log.error("RMI client failed: {}", e.getMessage());
         }
-
-        try {
-            log.info("Client asked Server to identify itself...");
-            log.info("SERVER => {}", vectorMath.identifyYourself());
-        } catch (RemoteException e) {
-            log.warn("Remote method invocation failed: {}", e.getMessage());
-            e.printStackTrace();
-        }
-
-        // Define two vectors
-        List<Float> v1 = List.of(9.8F, 5F, 10F, 1F, 1F);
-        log.info("Vector 1 = {}.", v1);
-        List<Float> v2 = List.of(0.2F, 5F, 17F, 1F, 9F);
-        log.info("Vector 2 = {}.\n\n", v2);
-
-        try {
-            VectorOperationResultDto vectorOperationResultDto = vectorMath.addition(v1, v2);
-            log.info("Received V1 = {}", vectorOperationResultDto.getV1());
-            log.info("Received V2 = {}", vectorOperationResultDto.getV2());
-            log.info("Addition = {}\n\n", vectorOperationResultDto.getResult());
-        } catch (RemoteException e) {
-            log.warn("Remote method invocation failed: {}", e.getMessage());
-            e.printStackTrace();
-        }
-
-        try {
-            VectorOperationResultDto vectorOperationResultDto = vectorMath.subtraction(v1, v2);
-            log.info("Received V1 = {}", vectorOperationResultDto.getV1());
-            log.info("Received V2 = {}", vectorOperationResultDto.getV2());
-            log.info("Subtraction = {}\n\n", vectorOperationResultDto.getResult());
-        } catch (RemoteException e) {
-            log.warn("Remote method invocation failed: {}", e.getMessage());
-            e.printStackTrace();
-        }
-
-        System.out.println("Conclusion: despite of the fact that Server modifies the vector's content, as they are passed by value and not by reference, the Client content won't be modified but the result will due to the Server is using those modified values! ");
     }
 
     private static VectorOperator getVectorMath(Registry registry) {
@@ -82,9 +89,8 @@ public class Client {
             log.info("Lookup success: remote object {} found at {}:{}", rmiObjectName, ipAddress, port);
         } catch (RemoteException e) {
             log.warn("Lookup failure: remote object '{}' NOT FOUND", rmiObjectName);
-            log.warn(e.getMessage());
         } catch (NotBoundException e) {
-            e.printStackTrace();
+            log.error("Lookup failure: remote object '{}' NOT BOUND", rmiObjectName);
         }
         return vectorMath;
     }
