dc48b1a0c342a1f888ed271c147829253d87edb7
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-21 00:19:29 -0300

    TP2-1: split in two modules.

--- TP2/TP2-1/Node/src/main/java/com/sdypp/node/NodeApplication.java
+++ TP2/TP2-1/Node/src/main/java/com/sdypp/node/NodeApplication.java
@@ -1,13 +1,13 @@
-package com.sdypp.code;
+package com.sdypp.node;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tp21Application {
+public class NodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Tp21Application.class, args);
+        SpringApplication.run(NodeApplication.class, args);
     }
 
 }
