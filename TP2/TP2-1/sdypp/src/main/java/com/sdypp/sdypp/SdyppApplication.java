bc03b3f01378ecfe817296fd23efb6793bfec0ed
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-12 21:31:27 -0300

    TP2-1: add REDIS dependency and config beans

--- TP2/TP2-1/sdypp/src/main/java/com/sdypp/sdypp/SdyppApplication.java
+++ TP2/TP2-1/sdypp/src/main/java/com/sdypp/sdypp/SdyppApplication.java
@@ -2,12 +2,32 @@ package com.sdypp.sdypp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class SdyppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SdyppApplication.class, args);
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConFactory
+                = new JedisConnectionFactory();
+        // Connects to redis default port 6379
+//        jedisConFactory.setHostName("localhost");
+//        jedisConFactory.setPort(6379);
+        return jedisConFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
     }
 
 }
