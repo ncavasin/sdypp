ff8a19cbc779302fa0fe057c8f2ff04fd058f9a5
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-12 22:29:32 -0300

    TP2-1: update ServerServiceImpl

--- TP2/TP2-1/sdypp/src/main/java/com/sdypp/sdypp/service/ServerServiceImpl.java
+++ TP2/TP2-1/sdypp/src/main/java/com/sdypp/sdypp/service/ServerServiceImpl.java
@@ -1,15 +1,24 @@
 package com.sdypp.sdypp.service;
 
+import com.sdypp.sdypp.domain.FileLocation;
 import com.sdypp.sdypp.dto.FileLocationDto;
 import com.sdypp.sdypp.dto.HelloDto;
+import com.sdypp.sdypp.repository.FileLocationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ServerServiceImpl implements ServerService {
+    private final FileLocationRepository fileLocationRepository;
 
     @Override
     public void hello(HelloDto helloDto) {
         // TODO
+        if(fileLocationRepository.existsById("")){
+            fileLocationRepository.save(new FileLocation());
+        }
+
 
         // Parse dto and hit redis
     }
