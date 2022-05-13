0ee1195d25c9282826513e7991c1c6aa807d63e4
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-12 22:28:18 -0300

    TP2-1: update REDIS dependency & add config class with beans

---
+++ TP2/TP2-1/sdypp/src/main/java/com/sdypp/sdypp/config/RedisConfiguration.java
@@ -0,0 +1,41 @@
+package com.sdypp.sdypp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfiguration {
+
+//    @Bean
+//    public JedisConnectionFactory redisConnectionFactory() {
+//
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("server", 6379);
+//        return new JedisConnectionFactory(config);
+//    }
+
+    @Bean
+    public RedisConnectionFactory connectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<byte[], byte[]>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(jedisConnectionFactory());
+//        return template;
+//    }
+}
