e877583d767767b856b7504f8cc1752b59660bb3
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-24 19:39:23 -0300

    TP2-1: add method for listing all entries

--- TP2/TP2-1/sdypp/src/main/java/com/sdypp/sdypp/service/ServerService.java
+++ TP2/TP2-1/sdypp/src/main/java/com/sdypp/sdypp/service/ServerService.java
@@ -1,9 +1,14 @@
 package com.sdypp.sdypp.service;
 
+import com.sdypp.sdypp.domain.FileLocation;
 import com.sdypp.sdypp.dto.FileOwnerDto;
 import com.sdypp.sdypp.dto.HelloDto;
 
+import java.util.List;
+
 public interface ServerService {
+
+    List<FileLocation> findAll();
 
     void hello(HelloDto helloDto);
 
