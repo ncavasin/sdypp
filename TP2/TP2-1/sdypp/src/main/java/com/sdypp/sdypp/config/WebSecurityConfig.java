a5931e2a6be7796d217d3dcc72c9030877838154
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-31 22:42:10 -0300

    TP2-1: enable CORS with security dependency

---
+++ TP2/TP2-1/sdypp/src/main/java/com/sdypp/sdypp/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.sdypp.sdypp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable();
+
+        // remove this to re-enable authentication
+        http.authorizeRequests().antMatchers("/**")
+                .permitAll();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedHeader("*");
+//        When allowCredentials is true, allowedOrigins cannot contain the special value "*"
+//        since that cannot be set on the "Access-Control-Allow-Origin" response header. To allow credentials to a set
+//        of origins, list them explicitly or consider using "allowedOriginPatterns" instead.
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
