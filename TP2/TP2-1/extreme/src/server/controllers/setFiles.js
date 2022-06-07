77b0feee9675cb81457054f6c1fcc014494920b0
FranZoia6 <39228851+FranZoia6@users.noreply.github.com>
2022-06-07 20:38:53 -0300

    cambios

--- TP2/TP2-1/extreme/src/server/controllers/setFiles.js
+++ TP2/TP2-1/extreme/src/server/controllers/setFiles.js
@@ -1,35 +1,59 @@
-const files = require("../files.json")
+//const files = require("../files.json")
+const fs = require('fs');
 const address = require("../address.json")
 const axios = require("axios");
 let index = 0;
+const { networkInterfaces } = require('os');
+var ip = require('ip');
 
+const nets = networkInterfaces();
+const results = Object.create(null);
 
+function httpQuery(method, endpoint, data) {
+    console.log(address[index]+endpoint);
+    return axios({
+        method,
+        url: "http://"+address[index]+endpoint,
+        data
 
-async function httpQuery(method, endpoint, data) {
-    try {
-        await axios({
-            method,
-            url: address[index] + endpoint,
-            data
-        })
-    }catch {
-        if (address.length > index) {
+    }).then(()=>{
+        console.log("/hello")
+        return true;
+    }).catch((e)=>{
+        console(e);
+        if ((address.length -1)> index) {
             index += 1;
-            httpQuery(method, endpoint, data)
+            return  httpQuery(method, endpoint, data)
         } else {
-            throw (new Error());
-
+            throw (new Error(e));
         }
-    }
+    })
+
 }
 
 
 const setFile = async (req, res) => {
     try {
-        await httpQuery("POST", "/api/p2p/hello", files)
-        res.render('index', {url:address[index]});
+         let dir = __dirname.replace('\\controllers','');
+        dir =dir + '/public/share/';
+        const names = fs.readdirSync(dir);
+        let arrayFiles = [];
+        for (let name of names) {
+                arrayFiles.push(name)
+              }
+        host = ip.address()+":3000";
+        console.log(host);
+        const files = {
+                "owner": host,
+                "files": arrayFiles
+        }
+        await httpQuery("POST","/api/p2p/hello", files)
+       return res.render('index', {url:address[index]});
     } catch (e) {
-        res.render('notfound');
+      info = address[index];
+
+        //return res.render('notfound');
+        return res.render('notfound', {respuesta:info});
     }
 }
 
--- TP2/TP2-1/extreme/src/server/index.js
+++ TP2/TP2-1/extreme/src/server/index.js
@@ -3,16 +3,28 @@ const express = require('express')
 const hbs = require('hbs');
 const app = express()
 const port = 3000
+app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
-
 // Exoress HBS engine
-hbs.registerPartials(__dirname + '/views');
+hbs.registerPartials(__dirname + 'views');
 app.set('view engine', 'hbs');
 
-// de esta manera importamos todo el contenido de un js
 //require('./hbs/helpers');
 
+
 app.get('/', setFile)
+
+app.get('/descargar/:id', (req,res)=>{
+  res.download(__dirname + '/public/share/'+req.params.id,
+  req.params.id,function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("listo")
+    }
+  } );
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
